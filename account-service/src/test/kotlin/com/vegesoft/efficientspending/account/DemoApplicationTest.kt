d2fb23454d2b09975da148f151479ed048e593de
Lukasz Rys <lukaszrys@me.com>
2020-02-09 16:20:05 +0100

    Init project with account-service

---
+++ account-service/src/test/kotlin/com/vegesoft/efficientspending/account/DemoApplicationTest.kt
@@ -0,0 +1,13 @@
+package com.vegesoft.efficientspending.account
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoApplicationTest {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
