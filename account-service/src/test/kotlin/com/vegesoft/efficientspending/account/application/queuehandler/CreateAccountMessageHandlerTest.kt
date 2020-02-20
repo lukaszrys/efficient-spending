26ea1b7d0c5753226ab410aa2990fb76ce55960b
Lukasz Rys <lukaszrys@me.com>
2020-02-21 00:42:53 +0100

    final adjustments

--- account-service/src/test/kotlin/com/vegesoft/efficientspending/account/application/queuehandler/CreateAccountMessageHandlerTest.kt
+++ account-service/src/test/kotlin/com/vegesoft/efficientspending/account/application/queuehandler/CreateAccountMessageHandlerTest.kt
@@ -1,5 +1,49 @@
 package com.vegesoft.efficientspending.account.application.queuehandler
 
-import org.junit.jupiter.api.Assertions.*
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.vegesoft.efficientspending.account.application.command.CreateAccountCommand
+import com.vegesoft.efficientspending.amqp.QueueNameProvider
+import com.vegesoft.efficientspending.amqp.QueueProperties
+import com.vegesoft.efficientspending.cqrs.CommandBus
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.RelaxedMockK
+import io.mockk.junit5.MockKExtension
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import kotlin.test.assertEquals
 
-internal class CreateAccountMessageHandlerTest
+@ExtendWith(MockKExtension::class)
+internal class CreateAccountMessageHandlerTest {
+
+    @RelaxedMockK
+    private lateinit var objectMapper: ObjectMapper
+    @RelaxedMockK
+    private lateinit var queueNameProvider: QueueNameProvider
+    @RelaxedMockK
+    private lateinit var queueProperties: QueueProperties
+    @RelaxedMockK
+    private lateinit var commandBus: CommandBus
+    @InjectMockKs
+    private lateinit var tested: CreateAccountMessageHandler
+
+    @Test
+    @DisplayName("Should return account service name")
+    fun shouldReturnAccountServiceName() {
+        val result = tested.getPropertyName()
+
+        assertEquals("accountService", result)
+    }
+
+    @Test
+    @DisplayName("Should dispatch message to commandBus")
+    fun shouldDispatchMessageToCommandBus() {
+        val message = mockk<CreateAccountCommand>()
+
+        tested.processMessage(message)
+
+        verify { commandBus.dispatch(message) }
+    }
+}
--- amqp-lib/src/test/kotlin/com/vegesoft/efficientspending/amqp/listener/AbstractQueueHandlerTest.kt
+++ amqp-lib/src/test/kotlin/com/vegesoft/efficientspending/amqp/listener/AbstractQueueHandlerTest.kt
@@ -1,3 +1,56 @@
 package com.vegesoft.efficientspending.amqp.listener
 
-internal class AbstractQueueHandlerTest
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.vegesoft.efficientspending.amqp.QueueNameProvider
+import com.vegesoft.efficientspending.amqp.QueueProperties
+import com.vegesoft.efficientspending.amqp.QueueType
+import io.mockk.every
+import io.mockk.junit5.MockKExtension
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import kotlin.test.assertTrue
+
+@ExtendWith(MockKExtension::class)
+internal class AbstractQueueHandlerTest {
+    private lateinit var objectMapper: ObjectMapper
+    private lateinit var queueNameProvider: QueueNameProvider
+    private lateinit var queueProperties: QueueProperties
+    private lateinit var serviceName: String
+    private lateinit var tested: ConcreteQueueHandler
+
+    @BeforeEach
+    fun setUp() {
+        objectMapper = mockk()
+        queueNameProvider = mockk()
+        queueProperties = mockk()
+        serviceName = "testService"
+        tested = ConcreteQueueHandler(objectMapper, queueNameProvider, queueProperties, serviceName)
+    }
+
+    @Test
+    @DisplayName("Should process the message")
+    fun shouldProcessTheMessage() {
+        val message = "message"
+
+        tested.handleMessage(message)
+
+        verify { objectMapper.readValue(message, QueueMessage::class.java) }
+    }
+
+    @Test
+    @DisplayName("Should could handle the message")
+    fun shouldCouldHandleTheMessage() {
+        val queueName = "queueName"
+
+        every { queueProperties.queues } returns mapOf(serviceName to queueName)
+        every { queueNameProvider.provideQueueName(queueName, QueueType.MAIN) } returns queueName
+
+        val result = tested.canHandle(queueName)
+
+        assertTrue { result }
+    }
+}
